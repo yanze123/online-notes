2a83f49e38c8daa5749a36725cd7401e5452d78a
yanze123 <462865314@qq.com>
2016-06-15 22:24:52 +0800

    login

---
+++ checkLogin.js
@@ -0,0 +1,18 @@
+function login(req, res, next) {
+	if(req.session.user) {
+		console.log('您已经登录！');
+		return res.redirect('back'); //返回之前的页面
+	}
+	next();
+}
+
+function noLogin(req,res,next) {
+	if(!req.session.user) {
+		console.log('抱歉，您还没有登录');
+		return res.redirect('/signin'); //返回登录页面
+	}
+	next();
+}
+
+exports.login = login;
+exports.noLogin = noLogin;
